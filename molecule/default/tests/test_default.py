e8a76206f96dc9a94b4cfb3adad0ebfcd27992fc
LinuxMalaysia <linuxmalaysia@songketmail.org>
2018-12-14 06:58:27 +0800

    test for .travis.yml

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,9 +1,22 @@
 import os
+import pytest
+
 
 import testinfra.utils.ansible_runner
 
+
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize('pkg', [
+  'lynis'
+])
+def test_pkg(host, pkg):
+    package = host.package(pkg)
+
+
+    assert package.is_installed
 
 
 def test_hosts_file(host):
