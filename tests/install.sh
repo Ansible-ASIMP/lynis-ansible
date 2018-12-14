dcdf1f9765b230e56ccc05b1c9062247eb149531
LinuxMalaysia <linuxmalaysia@songketmail.org>
2018-12-14 13:36:17 +0800

    Fix env issues

--- tests/install.sh
+++ tests/install.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-
-    # Install some custom requirements on Linux
-    # e.g. apt yum
-
-    case "${TOXENV}" in
-        ubuntu)
-            # Install some custom on Ubuntu
-            # Ansible doesn't play well with virtualenv
+if [[ $TRAVIS_OS_NAME == 'linux' ]]
+  then
+	 case "$TOXENV" in
+          ubuntu)
+            # Install some custom on Ubuntu Ansible doesn't play well with virtualenv
             deactivate
             curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
             sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
             sudo apt update -qq
             sudo apt -y -o Dpkg::Options::="--force-confnew" install docker-ce
-
             sudo pip install docker-py
             sudo apt install -y sshpass software-properties-common python-software-properties
             sudo apt-add-repository -y ppa:ansible/ansible
             sudo apt update -qq
             sudo apt-get install -y ansible
             ansible --version
-
-            ;;
-        centos)
-            # Install some custom on Centos
-            ;;
-    esac
-else
-    # Install some custom requirements on osx
+	    ;;
+	  centos)
+	    echo "Centos"
+	    ;;
+	 esac
+  else
+	  echo "other os"
 fi
