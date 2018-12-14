4cf378f14d7cd1ba4b0d634e79c546683031f909
LinuxMalaysia <linuxmalaysia@songketmail.org>
2018-12-14 22:50:57 +0800

    Path inside docker not correct

--- tests/b4install.sh
+++ tests/b4install.sh
@@ -23,8 +23,8 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]
             curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
             sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
             sudo apt update -qq
-            sudo apt upgrade -y
-            sudo apt dist-upgrade -y
+            #sudo apt upgrade -y
+            #sudo apt dist-upgrade -y
             sudo apt -y -o Dpkg::Options::="--force-confnew" install docker-ce
             docker --version
 	    sudo systemctl start docker
