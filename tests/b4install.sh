27ade9f7ba98497892dfacdc574d58d93a096b42
LinuxMalaysia <linuxmalaysia@songketmail.org>
2018-12-16 21:43:04 +0800

    Just for Debian

--- tests/b4install.sh
+++ tests/b4install.sh
@@ -25,8 +25,8 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]
             curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
             sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
             sudo apt update -qq
-            #sudo apt upgrade -y
-            #sudo apt dist-upgrade -y
+            sudo apt upgrade -y
+            sudo apt dist-upgrade -y
             sudo apt -y -o Dpkg::Options::="--force-confnew" install docker-ce
             docker --version
 	    echo 'DOCKER_OPTS="-H tcp://127.0.0.1:2375 -H unix:///var/run/docker.sock -s devicemapper"' | sudo tee /etc/default/docker > /dev/null
